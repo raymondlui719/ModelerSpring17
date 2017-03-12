5e10b8ad116af7531aa32f3be2849e9517e57e6d
Wong Chun Wai <christophermontgomery280@gmai..com>
2017-03-12 08:27:46 +0800

    Update of fltk package Implementation of simple model(hierarchy implemented)

--- sample.cpp
+++ sample.cpp
@@ -4,6 +4,7 @@
 #include "modelerapp.h"
 #include "modelerdraw.h"
 #include <FL/gl.h>
+#include <math.h>
 
 #include "modelerglobals.h"
 
@@ -34,38 +35,151 @@ void SampleModel::draw()
     ModelerView::draw();
 
 	// draw the floor
-	setAmbientColor(.1f,.1f,.1f);
+	setAmbientColor(.1f, .1f, .1f);
 	setDiffuseColor(COLOR_RED);
 	glPushMatrix();
-	glTranslated(-5,0,-5);
-	drawBox(10,0.01f,10);
+	glTranslated(-5, 0, -5);
+	drawBox(10, 0.01f, 10);
 	glPopMatrix();
 
 	// draw the sample model
-	setAmbientColor(.1f,.1f,.1f);
+	setAmbientColor(.1f, .1f, .1f);
 	setDiffuseColor(COLOR_GREEN);
 	glPushMatrix();
 	glTranslated(VAL(XPOS), VAL(YPOS), VAL(ZPOS));
 
-		glPushMatrix();
-		glTranslated(-1.5, 0, -2);
-		glScaled(3, 1, 4);
-		drawBox(1,1,1);
-		glPopMatrix();
 
-		// draw cannon
-		glPushMatrix();
-		glRotated(VAL(ROTATE), 0.0, 1.0, 0.0);
-		glRotated(-90, 1.0, 0.0, 0.0);
-		drawCylinder(VAL(HEIGHT), 0.1, 0.1);
+	//body
+	glPushMatrix();
+	glTranslated(-0.5, 2.25, -0.5);
+	glScaled(1, 1.5, 1);
+	drawBox(1, 1, 1);
+	glPopMatrix();
 
-		glTranslated(0.0, 0.0, VAL(HEIGHT));
-		drawCylinder(1, 1.0, 0.9);
+	//head
+	glPushMatrix();
+	glTranslated(0, 3.75, 0);
+	glScaled(0.5, 0.5, 0.5);
+	glRotated(-90, 1.0, 0.0, 0.0);
+	drawCylinder(1, 1, 1);
+	glPopMatrix();
 
-		glTranslated(0.0, 0.0, 0.5);
-		glRotated(90, 1.0, 0.0, 0.0);
-		drawCylinder(4, 0.1, 0.2);
-		glPopMatrix();
+	//rightArm
+	//shoulder
+	glPushMatrix();
+	glTranslated(-0.75, 3.5, 0);
+	drawSphere(0.25);
+	//uArm
+	glPushMatrix();
+	glTranslated(-0.25, -0.75, -0.25);
+	glScaled(0.5, 0.75, 0.5);
+	drawBox(1, 1, 1);
+	glPopMatrix();
+	//elbow
+	glTranslated(0, -0.85, 0);
+	drawSphere(0.25);
+	//lArm
+	glTranslated(-0.25, -0.85, -0.25);
+	glScaled(0.5, 0.75, 0.5);
+	drawBox(1, 1, 1);
+	glPopMatrix();
+
+	//leftArm
+	//shoulder
+	glPushMatrix();
+	glTranslated(0.75, 3.5, 0);
+	drawSphere(0.25);
+	//uArm
+	glPushMatrix();
+	glTranslated(-0.25, -0.75, -0.25);
+	glScaled(0.5, 0.75, 0.5);
+	drawBox(1, 1, 1);
+	glPopMatrix();
+	//elbow
+	glTranslated(0, -0.85, 0);
+	drawSphere(0.25);
+	//lArm
+	glTranslated(-0.25, -0.85, -0.25);
+	glScaled(0.5, 0.75, 0.5);
+	drawBox(1, 1, 1);
+	glPopMatrix();
+	
+	//lowerBody
+	glPushMatrix();
+	glTranslated(0, 2.15, 0);
+	glPushMatrix();
+	glScaled(0.5, 0.1, 0.5);
+	glRotated(-90, 1.0, 0.0, 0.0);
+	drawCylinder(1, 1, 1);
+	glPopMatrix();
+	//glPopMatrix();
+
+	//leftLeg
+	//uArm
+	glPushMatrix();
+	//ankle
+	glTranslated(-0.25, -0.1, 0);
+	drawSphere(0.25);
+	//lArm
+	glPushMatrix();
+	glTranslated(-0.25, -1.1, -0.25);
+	glScaled(0.45, 1, 0.45);
+	drawBox(1, 1, 1);
+	glPopMatrix();
+	//elbow
+	glTranslated(0, -1.1, 0);
+	drawSphere(0.25);
+	//lArm
+	glTranslated(-0.25, -0.9, -0.25);
+	glScaled(0.45, 0.75, 0.45);
+	drawBox(1, 1, 1);
+	glPopMatrix();
+
+
+
+	//rightLeg
+	//uArm
+	glPushMatrix();
+	//ankle
+	glTranslated(0.25, -0.1, 0);
+	drawSphere(0.25);
+	//lArm
+	glPushMatrix();
+	glTranslated(-0.25, -1.1, -0.25);
+	glScaled(0.45, 1, 0.45);
+	drawBox(1, 1, 1);
+	glPopMatrix();
+	//elbow
+	glTranslated(0, -1.1, 0);
+	drawSphere(0.25);
+	//lArm
+	glTranslated(-0.25, -0.9, -0.25);
+	glScaled(0.45, 0.75, 0.45);
+	drawBox(1, 1, 1);
+	glPopMatrix();
+	
+	glPopMatrix();
+	//lowerBodyEnd
+
+
+	
+	
+	
+	/*
+	// draw cannon
+	glPushMatrix();
+	glRotated(VAL(ROTATE), 0.0, 1.0, 0.0);
+	glRotated(-90, 1.0, 0.0, 0.0);
+	drawCylinder(VAL(HEIGHT), 0.1, 0.1);
+
+	glTranslated(0.0, 0.0, VAL(HEIGHT));
+	drawCylinder(1, 1.0, 0.9);
+
+	glTranslated(0.0, 0.0, 0.5);
+	glRotated(90, 1.0, 0.0, 0.0);
+	drawCylinder(4, 0.1, 0.2);
+	glPopMatrix();
+	*/
 
 	glPopMatrix();
 }
